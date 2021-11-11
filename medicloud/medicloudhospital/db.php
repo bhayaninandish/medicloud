fabcb931481cb0e00d7a3ba8d68aea37a8582985
Nandish Bhayani <nandish5920@gmail.com>
2021-11-11 17:27:55 +0530

    Delete medicloud directory

--- medicloud/medicloudhospital/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn = new mysqli("localhost","nandish","password","hospital");
-
-if($conn->connect_error){
-    die('Database error'. $conn->connect_error);
-}
--- medicloud/medicloudhospital/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["aadhar"]);
-    unset($_SESSION["password"]);
-    unset($_SESSION["logged_in"]);
-    header("Location:login.php");
-    session_destroy();
-?>
